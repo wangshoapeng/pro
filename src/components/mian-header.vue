1d54fad2db6eeb3e153679c55fdc997b234a5227
wangshaopeng <815151108@qq.com>
2018-12-29 18:40:02 +0800

    00

--- src/components/mian-header.vue
+++ src/components/mian-header.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="mian-header">
-    <span @click="$router.go(-1)" class="goback-icon" v-if="back">
+    <span @click="$router.go(-1)" class="header-left-icon" v-if="back">
       <i class="iconfont x-fanhui"></i>
       返回</span>
-    <span class="header-more">
+    <span @click="$router.go(0)" class="header-left-icon" style="marginLeft:.2rem" v-else>
+      <i class="iconfont x-shuaxin"></i>
+    </span>
+    <span class="header-more-icon">
       <i class="iconfont x-gengduo"></i></span>
     <slot></slot>
   </div>
@@ -33,14 +36,16 @@ export default {
   margin-bottom: 0.24rem;
   border-radius: 0.05rem;
 }
-.goback-icon {
-  float: left;
+.header-left-icon {
+  position: absolute;
+  left: 0;
   color: white;
   font-weight: 300;
   padding: 0 0.1rem;
 }
-.header-more {
-  float: right;
+.header-more-icon {
+  position: absolute;
+  right: 0;
   color: white;
   font-size: 0.48rem;
   padding: 0 0.4rem;
