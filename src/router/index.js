e518b70e86e52c57c6168828a3845e1c3db195c4
wangshaopeng <815151108@qq.com>
2018-12-30 21:09:16 +0800

    kl

--- src/router/index.js
+++ src/router/index.js
@@ -4,56 +4,55 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '*',
-      redirect: '/'
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/view/Home.vue')
-    },
+  routes: [{
+    path: '*',
+    redirect: '/'
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/view/Home.vue')
+  },
 
-    {
-      path: '/studynav',
-      name: 'studynav',
-      component: () => import('@/view/study-nav.vue')
-    },
-    {
-      path: '/prop',
-      name: 'prop',
-      component: () => import('@/components/props/prop-father.vue')
-    },
-    {
-      path: '/emit',
-      name: 'emit',
-      component: () => import('@/components/emit/EmitA.vue')
-    },
-    {
-      path: '/alert',
-      name: 'alert',
-      component: () => import('@/view/AlertPage.vue')
-    },
-    {
-      path: '/alertMin',
-      name: 'alertMIn',
-      component: () => import('@/view/AlertPageMin.vue')
-    },
-    {
-      path: '/propsarray',
-      name: 'propsarray',
-      component: () => import('@/xiaoce/propsarray.vue')
-    },
-    {
-      path: '/transfrom',
-      name: 'transfrom',
-      component: () => import('../view/Transfrom.vue')
-    },
-    {
-      path: '/iviewA',
-      name: 'iviewA',
-      component: () => import('../view/IviewA.vue')
-    }
+  {
+    path: '/studynav',
+    name: 'studynav',
+    component: () => import('@/view/study-nav.vue')
+  },
+  {
+    path: '/prop',
+    name: 'prop',
+    component: () => import('@/components/props/prop-father.vue')
+  },
+  {
+    path: '/emit',
+    name: 'emit',
+    component: () => import('@/components/emit/EmitA.vue')
+  },
+  {
+    path: '/alert',
+    name: 'alert',
+    component: () => import('@/view/AlertPage.vue')
+  },
+  {
+    path: '/alertMin',
+    name: 'alertMIn',
+    component: () => import('@/view/AlertPageMin.vue')
+  },
+  {
+    path: '/propsarray',
+    name: 'propsarray',
+    component: () => import('@/xiaoce/propsarray.vue')
+  },
+  {
+    path: '/transfrom',
+    name: 'transfrom',
+    component: () => import('../view/Transfrom.vue')
+  },
+  {
+    path: '/iviewA',
+    name: 'iviewA',
+    component: () => import('../view/IviewA.vue')
+  }
   ]
 })
